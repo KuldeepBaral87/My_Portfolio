17da0c882af6642448c8a701379308c93af947e9
baralKuldeep87 <baralkuldeep7@gmail.com>
2023-08-16 00:31:42 +0530

    Trying GSAP

---
+++ script.js
@@ -0,0 +1,36 @@
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.set("innercontainer",{
+    opacity:1,duration:2,
+});
+gsap.set("innercontainer2",{
+    opacity:1,duration:2,
+});
+gsap.from("innercontainer",{
+    x:-100,
+    duration:2,
+    opacity:0,
+    scrollTrigger:"innercontainer",
+});
+gsap.from("innercontainer2",{
+    x:-200,
+    duration:2,
+    opacity:0,
+    scrollTrigger:"innercontainer2",
+});
+gsap.set("container h1",{
+    opacity:1,duration:2,
+});
+gsap.from("container h1",{
+    y:-100,
+    duration:2,
+    opacity:0,
+    scrollTrigger:"container h1",
+});
+
+
+gsap.from("Hobby1",{
+    x:50+"vw",
+    duration:1,
+    scrollTrigger:"Hobby1"
+});
